2a63e8c0d988df6531ad7fe8bbcb5d889ddd1641
nitesh <nitesh.barot31@gmail.com>
2023-09-18 11:32:03 +0530

    New Changes

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -3,6 +3,7 @@ package factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -18,8 +19,11 @@ public class DriverFactory {
         System.out.println("The name of the browser is " + browser);
 
         if (browser.equals("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            tlLocal.set(new ChromeDriver());
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless", "--window-size=1920,1200");
+
+            WebDriver driver1=new ChromeDriver(options);
+            tlLocal.set(driver1);
 
 
         }
